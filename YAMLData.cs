c2aa4766a66867497e209b4a7201d89a072e8cae
Andrew Nelson <evildarkarchon@gmail.com>
2024-05-27 04:22:59 -0700

    Made all YAMLCache instantiations in YAMLData static so that the YAMLData class won't need to be instantiated as well.

--- YAMLData.cs
+++ YAMLData.cs
@@ -2,20 +2,16 @@
 {
     public class YAMLData
     {
-        public YamlCache CLASSIC_Main { get; set; } = new YamlCache("CLASSIC Data/databases/CLASSIC Main.yaml");
-        public YamlCache CLASSIC_Fallout4 { get; set; } = new YamlCache("CLASSIC Data/databases/CLASSIC Fallout4.yaml");
-        public YamlCache CLASSIC_Settings { get; set; } = new YamlCache("CLASSIC Settings.yaml");
-        public YamlCache CLASSIC_Ignore { get; set; } = new YamlCache("CLASSIC Ignore.yaml");
-        public YamlCache CLASSIC_FO4_Local { get; set; } = new YamlCache("CLASSIC Data/CLASSIC Fallout4 Local.yaml");
-        public YamlCache CLASSIC_FO4 { get; set; } = new YamlCache("CLASSIC Data/databases/CLASSIC Fallout4.yaml");
+        public static YamlCache CLASSIC_Main { get; set; } = new YamlCache("CLASSIC Data/databases/CLASSIC Main.yaml");
+        public static YamlCache CLASSIC_Fallout4 { get; set; } = new YamlCache("CLASSIC Data/databases/CLASSIC Fallout4.yaml");
+        public static YamlCache CLASSIC_Settings { get; set; } = new YamlCache("CLASSIC Settings.yaml");
+        public static YamlCache CLASSIC_Ignore { get; set; } = new YamlCache("CLASSIC Ignore.yaml");
+        public static YamlCache CLASSIC_FO4_Local { get; set; } = new YamlCache("CLASSIC Data/CLASSIC Fallout4 Local.yaml");
+        public static YamlCache CLASSIC_FO4 { get; set; } = new YamlCache("CLASSIC Data/databases/CLASSIC Fallout4.yaml");
 
-        public dynamic? SettingsCheck(string key)
+        public static dynamic? SettingsCheck(string key)
         {
             return CLASSIC_Settings.ReadOrUpdateEntry($"CLASSIC_Settings.{key}");
         }
-    }
-    public class YamlInstance
-    {
-        public static YAMLData Data { get; set; } = new YAMLData();
     }
 }
