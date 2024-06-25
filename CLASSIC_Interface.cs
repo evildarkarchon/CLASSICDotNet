7d953354f2097c73a973a1faf79d09fcd4b201d0
Andrew Nelson <evildarkarchon@gmail.com>
2024-06-25 03:18:32 -0700

    Restructuring

---
+++ CLASSIC_Interface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLASSICDotNet
+{
+    public partial class CLASSIC_Interface : Form
+    {
+        public CLASSIC_Interface()
+        {
+            InitializeComponent();
+        }
+    }
+}
